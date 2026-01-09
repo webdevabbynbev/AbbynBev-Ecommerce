82dbcff3378d1acafb3b26b7ceb5d3003208f1db
presidentanjay <gentalav54@gmail.com>
2026-01-09 11:25:04 +0700

    update

--- cmsadmin-ab/src/pages/MasterPage.tsx
+++ cmsadmin-ab/src/pages/MasterPage.tsx
@@ -27,6 +27,7 @@ import TableProduct from "../components/Tables/Product/TableProduct";
 import TableTransaction from "../components/Transaction/TableTransaction";
 import TableRamadanEvent from "../components/Tables/Ramadan/TableRamadanEvent";
 import TableSale from "../components/Tables/Sale/TableSale";
+import TableRamadanRecommendation from "../components/Tables/Ramadan/TableRamadanEvent";
 
 export default function MasterPage(): React.ReactElement {
   return (
